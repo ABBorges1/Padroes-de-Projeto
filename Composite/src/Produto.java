554904d249f694174e4dab59557b1f9f0b1879b0
ABBorges1 <adrielbarborges@gmail.com>
2022-11-24 21:06:03 -0300

    Padrão de Projeto - COMPOSITE

---
+++ Composite/src/Produto.java
@@ -0,0 +1,15 @@
+public class Produto implements Item{
+	private String nome;
+	private double preco;
+	
+	public Produto(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	@Override
+	public double calcularPrecoFinal() {
+		return preco;
+	}
+	
+}
